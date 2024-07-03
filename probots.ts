aca9f7b5678fde65e9e0460e0443a376d55069f1
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-07-03 16:22:59 -0300

    change visible name of block

--- probots.ts
+++ probots.ts
@@ -4,7 +4,7 @@
 /**
  * Probots version 1.
  */
-//% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
+//% color=#ff8000 weight=100 icon="\uf2db" block="Probots V1"
 namespace probots{
     
 }
