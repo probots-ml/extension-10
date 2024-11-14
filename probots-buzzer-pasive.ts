639526c2b0a0336d744e853a2e1d9e7209ecbfba
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-11-14 13:09:16 -0300

    minors changes

--- probots-buzzer-pasive.ts
+++ probots-buzzer-pasive.ts
@@ -3,6 +3,7 @@ namespace probots {
     //% block="play %note=BUZZER_TONE by $time ms on port %myPort=brickPortPitch"    
     //% subcategory="Passive Buzzer"
     //% color=#A31298
+    //% weight=100
     export function buzzerPasiveOn(note: any, time: number, myPort: any) {
         pins.analogSetPitchPin(myPort.P1);
         pins.analogPitch(note, time);
