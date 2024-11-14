b3c6d4088e099874c29580e5fcb910889a3f1f94
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-11-14 13:02:20 -0300

    Passive buzzer

--- probots-buzzer-pasive.ts
+++ probots-buzzer-pasive.ts
@@ -1,16 +1,6 @@
 namespace probots {
     
-    /**
-     * Passive Buzzer. Play a note during a selected miliseconds time.
-     * @param note Select the Tone note
-     * @param time Set the duration in miliseconds
-     * @param myPort Select the port to connect the pasive Buzzer.
-     */
-
-    //% block="play %note=BUZZER_TONE by $time ms on %myPort=brickPortPitch
-    //% note.defl=BUZZER_TONE.A
-    //% time.min=10 time.max=5000 time.defl=1000
-    //% weight=100
+  
     //% subcategory="Passive Buzzer"
     //% color=#A31298
     export function buzzerPasiveOn(note: any, time: number, myPort: any) {
